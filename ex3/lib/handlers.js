ddd680075f20a4a9699514f7403274e006c11ed8
Richard L. Jackson <denezt@yahoo.com>
2020-12-27 16:14:04 +0100

    Updating code - 1609082044

--- ex3/lib/handlers.js
+++ ex3/lib/handlers.js
@@ -97,7 +97,8 @@ handlers._users.post = function(data,callback){
 // Optional data: none
 // @TODO Only let an authenticated user access their object. Dont let them access anyone elses.
 handlers._users.get = function(data, callback){
-  var datastoreFilename = handlers.datastore(data.payload.emailAddress);
+  // var datastoreFilename = handlers.datastore(data.payload.emailAddress);
+  console.log(data);
   callback(200,data);
     // Lookup the user
     // _data.read('users',datastoreFilename,function(err,data){
