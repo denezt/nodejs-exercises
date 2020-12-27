6fedea763fecf624ab3d437781578266cb303ade
Richard L. Jackson <denezt@yahoo.com>
2020-12-27 17:04:42 +0100

    Updating code - 1609085082

--- ex3/testing/data_wrapper.sh
+++ ex3/testing/data_wrapper.sh
@@ -17,6 +17,8 @@ curl --location --request POST '139.59.147.182:3000/users' \
 }
 
 get_data(){
+	# curl --location --request GET \
+	# '139.59.147.182:3000/users?emailAddress=myemail@email.com'
 	curl --location --request GET \
 	'139.59.147.182:3000/users?emailAddress=myemail@email.com'
 }
