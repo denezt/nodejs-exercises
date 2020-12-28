a35bad3a94ff82625214cb9916ead44532f003fe
Richard L. Jackson <denezt@yahoo.com>
2020-12-28 20:50:40 +0100

    Updating code - 1609185040

--- ex3/index.js
+++ ex3/index.js
@@ -101,7 +101,7 @@ var unifiedServer = function(req,res){
 
 // Define the request router
 var router = {
-  'ping' : handlers.ping,
-  'users' : handlers.users,
-  'tokens' : handlers.tokens
+  'ping' : handlers.ping
+  // 'users' : handlers.users,
+  // 'tokens' : handlers.tokens
 };
