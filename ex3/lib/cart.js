59ed75b542f874f92d698723404631464cf524ef
Richard L. Jackson <denezt@yahoo.com>
2021-01-01 21:42:19 +0100

    Updating code - 1609533739

--- ex3/lib/cart.js
+++ ex3/lib/cart.js
@@ -35,6 +35,11 @@ cart._cart.post = function(data,callback){
       token_holder._token.verifyToken(token, emailAddress, function(tokenIsValid){
         if (tokenIsValid){
           var cartName = helper.hash128(emailAddress);
+          itemObject = {
+            "items":[
+              {'':''}
+            ]
+          }
           // Get the token from the headers
           _data.read('carts',cartName,function(err,data){
             if(err){
@@ -61,6 +66,68 @@ cart._cart.post = function(data,callback){
   }
 };
 
+
+
+// Required data: emailAddress
+// Optional data: firstName, lastName, password (at least one must be specified)
+cart._cart.put = function(data,callback){
+  var emailAddress = data.payload.emailAddress;
+  var datastoreFilename = typeof(helper.datastore(emailAddress)) == 'string' && emailAddress.trim().length > 0 ? helper.datastore(emailAddress) : false;
+
+  // Check for optional fields
+  var itemId = typeof(data.payload.itemid) == 'string' && data.payload.itemid.trim().length > 0 ? data.payload.itemid.trim() : false;
+
+
+  if (datastoreFilename){
+    // Error if nothing is sent to update
+    if(itemId && itemCount){
+
+      // Get the token from the headers
+      var token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
+        // Verify that the given token is valid for the email
+        token_holder._token.verifyToken(token, emailAddress, function(tokenIsValid){
+            if (tokenIsValid){
+              var cartName = helper.hash128(emailAddress);
+
+              // Lookup the user
+              _data.read('carts',cartName, function(err,userData){
+                if(!err && userData){
+                  for (var i = 0; i < userData.itemList.length; i++) {
+                    console.log(userData.itemList[i]);
+
+                  }
+                  // Update the fields if necessary
+                  if(firstName){
+                    userData.firstName = firstName;
+                  }
+
+                  // Store the new updates
+                  _data.update('carts',datastoreFilename,userData,function(err){
+                    if(!err){
+                      callback(200);
+                    } else {
+                      console.log(err);
+                      callback(500,{'Error' : 'Could not update the user.'});
+                    }
+                  });
+                } else {
+                  callback(400,{'Error' : 'Specified user does not exist.'});
+                }
+              });
+            }else{
+              callback(403,{'Error':'Missing required token in header, or token is invalid'});
+            }
+        });
+
+      }
+  } else {
+    callback(400,{'Error' : 'Missing required field or parameters are incorrect.'});
+  }
+};
+
+
+// Required data: emailAddress
+// Optional data: None
 cart._cart.get = function(data,callback){
   console.log(data.payload);
   var emailAddress = typeof(data.payload.emailAddress) == 'string' ? data.payload.emailAddress : false;
