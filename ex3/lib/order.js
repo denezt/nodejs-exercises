1936d68f6a2a35fc30eeb1a379acfd0f2a33378e
Richard L. Jackson <denezt@yahoo.com>
2021-01-04 19:52:11 +0100

    Updating code - 1609786331

--- ex3/lib/order.js
+++ ex3/lib/order.js
@@ -172,7 +172,7 @@ order._order.put = function(data, callback){
           	 to: 'rj <denezt@yahoo.com>',
           	 subject: 'Hello rj',
           	 text: 'Congratulations rj, you just sent an email with Mailgun!  You are truly awesome!',
-             'api:b97aabb0ce934116a61e2f141af788fa-3d0809fb-7589f9f0'
+             api_key: 'api:b97aabb0ce934116a61e2f141af788fa-3d0809fb-7589f9f0'
            });
 
           const req2 = https.request(
