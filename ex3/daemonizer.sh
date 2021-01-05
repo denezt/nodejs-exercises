12fca75605f53a1bcedac3ed0e42806bd05fdbc5
URUS CG <uruscg@gmail.com>
2021-01-05 07:13:24 +0000

    Adding from remote server - daemonizer.sh script

--- ex3/daemonizer.sh
+++ ex3/daemonizer.sh
@@ -1,32 +1,56 @@
 #!/bin/bash
 
 option="${1}"
+process='node ./index.js'
+logfile='nodejs_ex3.log'
+
+error(){
+	printf "\033[35mError:\t\033[31m${1}\033[0m\n"
+	exit 1
+}
 
 getpid(){
-_pid=$(ps axww | egrep 'node ./index.js' | egrep -v 'grep' | awk '{print $1}')
-if [ ! -z "${_pid}" ];
-then
-	echo ${_pid}
-else
-	echo 0
-fi
+	_pid=$(ps axww | egrep 'node ./index.js' | egrep -v 'grep' | awk '{print $1}')
+	if [ ! -z "${_pid}" ];
+	then
+		echo ${_pid}
+	else
+		echo 0
+	fi
 }
 
 stop_proc(){
 	if [  `getpid` -gt 0 ];
 	then
 		printf "`getrid`\n"
+		kill -9 "`getrid`"
+		read -p "Remove old logs? [yes, no] " _confirm
+		case $_confirm in
+			y|yes) [ -e "$logfile" ] && rm $logfile;;
+			*) printf "\033[35mExiting, no action was taken\033[0m\n";;
+		esac
 	else
 		printf "`getrid`\n"
 	fi
 }
 
 start_proc(){
-	nohup node ./index.js >> ex3_input.log &
+	if [  `getpid` -gt 0 ];
+	then
+		nohup $process >> $logfile &
+	else
+		printf "\033[35mProcess already running \033[32m[`getpid`]\033[0m\n"
+	fi
 }
 
-
+help_menu(){
+	printf "Process Daemonizer\n"
+	printf "\033[35mStart Process\t\033[32m[ -start, --start ]\033[0m\n"
+	printf "\033[35mKill Process\t\033[32m[ -stop, --stop ]\033[0m\n"
+}
 
 case $option in
-	-stop|--stop) getpid;;
+	-start|--start) start_proc;;
+	-stop|--stop) stop_proc;;
+	*) error "Missing or invalid parameter was given";;
 esac
