20e9a6a07dcf572824e2ba19bc521c4470c1152c
Richard L. Jackson <denezt@yahoo.com>
2021-01-07 22:35:18 +0100

    Updating code - 1610055318

--- ex4/lib/handlers.js
+++ ex4/lib/handlers.js
@@ -12,6 +12,24 @@ var helper = require('./helper');
 // Define all the handlers
 var handlers = {};
 
+// Index Handler
+handlers.index = function(data,callback){
+  // Reject any request that isn't a GET
+  if(data.method == 'get'){
+    // Read in a template as a string
+    helpers.getTemplate('index',function(err,str){
+      if(!err && str){
+        callback(200,str,'html');
+      } else {
+        callback(500,undefined,'html')
+      }
+    });
+    // Return that template as HTML
+  } else {
+    callback(405,undefined,'html');
+  }
+};
+
 // Ping
 handlers.ping = function(data,callback){
   callback(200,{'status':'ok'});
