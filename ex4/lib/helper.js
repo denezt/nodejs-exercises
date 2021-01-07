c73ffbe7abd11ed4ba6641cbd0cba72a3f17d44b
Richard L. Jackson <denezt@yahoo.com>
2021-01-07 22:37:49 +0100

    Updating code - 1610055469

--- ex4/lib/helper.js
+++ ex4/lib/helper.js
@@ -70,5 +70,22 @@ helper.datastore = function(data, callback){
   }
 };
 
+// Get the string content of a template
+helper.getTemplate = function(templateName,callback){
+  templateName = typeof(templateName) == 'string' && templateName.length > 0 ? templateName : false;
+  if(templateName){
+    var templatesDir = path.join(__dirname,'/../templates/');
+    fs.readFile(templatesDir+templateName+'.html', 'utf8', function(err,str){
+      if(!err && str && str.length > 0){
+        callback(false,str);
+      } else {
+        callback('No template could be found');
+      }
+    });
+  } else {
+    callback('A valid template name was not specified');
+  }
+};
+
 // Export the module
 module.exports = helper;
