f8bce4ea0bf5c95580037bcbeb273eeb103beebf
Richard L. Jackson <denezt@yahoo.com>
2021-01-08 17:53:45 +0100

    Updating code - 1610124825

---
+++ ex4/lib/account.js
@@ -0,0 +1,39 @@
+var _data = require('./data');
+var helper = require('./helper');
+var config = require('./config');
+
+
+
+var account = {}
+
+
+// Create Account
+account.accountCreate = function(data,callback){
+  // Reject any request that isn't a GET
+  if(data.method == 'get'){
+    // Prepare data for interpolation
+    var templateData = {
+      'head.title' : 'Create an Account',
+      'head.description' : 'Signup is easy and only takes a few seconds.',
+      'body.class' : 'accountCreate'
+    };
+    // Read in a template as a string
+    helper.getTemplate('accountCreate',templateData,function(err,str){
+      if(!err && str){
+        // Add the universal header and footer
+        helper.addUniversalTemplates(str,templateData,function(err,str){
+          if(!err && str){
+            // Return that page as HTML
+            callback(200,str,'html');
+          } else {
+            callback(500,undefined,'html');
+          }
+        });
+      } else {
+        callback(500,undefined,'html');
+      }
+    });
+  } else {
+    callback(405,undefined,'html');
+  }
+};
