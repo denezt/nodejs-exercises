fa86980bd837252f87c664c6ed737b089329f9e3
Richard L. Jackson <denezt@yahoo.com>
2021-01-08 18:48:08 +0100

    Updating code - 1610128088

--- ex4/lib/config.js
+++ ex4/lib/config.js
@@ -16,7 +16,7 @@ environments.staging = {
     'appName' : 'UptimeChecker',
     'companyName' : 'NotARealCompany, Inc.',
     'yearCreated' : '2018',
-    'baseUrl' : 'http://localhost:5000/'
+    'baseUrl' : 'http://localhost:3000/'
   }
 };
 
