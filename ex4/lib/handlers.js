ef8d15dbe398dfa82e4d6c84961ec1f373389300
Richard L. Jackson <denezt@yahoo.com>
2021-01-13 22:32:14 +0100

    Updating code - 1610573534

--- ex4/lib/handlers.js
+++ ex4/lib/handlers.js
@@ -232,17 +232,17 @@ handlers.orderCreate = function(data,callback){
   }
 };
 
-// Dashboard (view all checks)
-handlers.checksList = function(data,callback){
+// Dashboard (view all order)
+handlers.orderList = function(data,callback){
   // Reject any request that isn't a GET
   if(data.method == 'get'){
     // Prepare data for interpolation
     var templateData = {
       'head.title' : 'Dashboard',
-      'body.class' : 'checksList'
+      'body.class' : 'orderList'
     };
     // Read in a template as a string
-    helpers.getTemplate('checksList',templateData,function(err,str){
+    helpers.getTemplate('orderList',templateData,function(err,str){
       if(!err && str){
         // Add the universal header and footer
         helpers.addUniversalTemplates(str,templateData,function(err,str){
@@ -263,16 +263,16 @@ handlers.checksList = function(data,callback){
 };
 
 // Edit a Check
-handlers.checksEdit = function(data,callback){
+handlers.orderEdit = function(data,callback){
   // Reject any request that isn't a GET
   if(data.method == 'get'){
     // Prepare data for interpolation
     var templateData = {
       'head.title' : 'Check Details',
-      'body.class' : 'checksEdit'
+      'body.class' : 'orderEdit'
     };
     // Read in a template as a string
-    helpers.getTemplate('checksEdit',templateData,function(err,str){
+    helpers.getTemplate('orderEdit',templateData,function(err,str){
       if(!err && str){
         // Add the universal header and footer
         helpers.addUniversalTemplates(str,templateData,function(err,str){
@@ -570,7 +570,7 @@ handlers._users.put = function(data,callback){
 };
 
 // Required data: emailAddress
-// Cleanup old checks associated with the user
+// Cleanup old order associated with the user
 handlers._users.delete = function(data,callback){
   // Check that emailAddress number is valid
   var emailAddress = typeof(data.payload.emailAddress) == 'string' && data.payload.emailAddress.trim().length > 1 && (data.payload.emailAddress.indexOf('@') > -1 ) ? helpers.datastore(data.payload.emailAddress.trim()) : false;
@@ -589,25 +589,25 @@ handlers._users.delete = function(data,callback){
             // Delete the user's data
             _data.delete('users',emailAddress,function(err){
               if(!err){
-                // Delete each of the checks associated with the user
-                var userChecks = typeof(userData.checks) == 'object' && userData.checks instanceof Array ? userData.checks : [];
-                var checksToDelete = userChecks.length;
-                if(checksToDelete > 0){
-                  var checksDeleted = 0;
+                // Delete each of the order associated with the user
+                var userChecks = typeof(userData.order) == 'object' && userData.order instanceof Array ? userData.order : [];
+                var orderToDelete = userChecks.length;
+                if(orderToDelete > 0){
+                  var orderDeleted = 0;
                   var deletionErrors = false;
-                  // Loop through the checks
+                  // Loop through the order
                   userChecks.forEach(function(checkId){
                     // Delete the check
-                    _data.delete('checks',checkId,function(err){
+                    _data.delete('order',checkId,function(err){
                       if(err){
                         deletionErrors = true;
                       }
-                      checksDeleted++;
-                      if(checksDeleted == checksToDelete){
+                      orderDeleted++;
+                      if(orderDeleted == orderToDelete){
                         if(!deletionErrors){
                           callback(200);
                         } else {
-                          callback(500,{'Error' : "Errors encountered while attempting to delete all of the user's checks. All checks may not have been deleted from the system successfully."})
+                          callback(500,{'Error' : "Errors encountered while attempting to delete all of the user's order. All order may not have been deleted from the system successfully."})
                         }
                       }
                     });
@@ -787,24 +787,24 @@ handlers._tokens.verifyToken = function(id,emailAddress,callback){
   });
 };
 
-// Checks
-handlers.checks = function(data,callback){
+// Order
+handlers.order = function(data,callback){
   var acceptableMethods = ['post','get','put','delete'];
   if(acceptableMethods.indexOf(data.method) > -1){
-    handlers._checks[data.method](data,callback);
+    handlers._order[data.method](data,callback);
   } else {
     callback(405);
   }
 };
 
-// Container for all the checks methods
-handlers._checks  = {};
+// Container for all the order methods
+handlers._order  = {};
 
 
 // Checks - post
 // Required data: protocol,url,method,successCodes,timeoutSeconds
 // Optional data: none
-handlers._checks.post = function(data,callback){
+handlers._order.post = function(data,callback){
   // Validate inputs
   var protocol = typeof(data.payload.protocol) == 'string' && ['https','http'].indexOf(data.payload.protocol) > -1 ? data.payload.protocol : false;
   var url = typeof(data.payload.url) == 'string' && data.payload.url.trim().length > 0 ? data.payload.url.trim() : false;
@@ -819,21 +819,19 @@ handlers._checks.post = function(data,callback){
     // Lookup the user emailAddress by reading the token
     _data.read('tokens',token,function(err,tokenData){
       if(!err && tokenData){
-        var userPhone = tokenData.emailAddress;
+        var userEmail = tokenData.emailAddress;
 
         // Lookup the user data
-        _data.read('users',userPhone,function(err,userData){
+        _data.read('users',userEmail,function(err,userData){
           if(!err && userData){
-            var userChecks = typeof(userData.checks) == 'object' && userData.checks instanceof Array ? userData.checks : [];
-            // Verify that user has less than the number of max-checks per user
-            if(userChecks.length < config.maxChecks){
+            var userOrder = typeof(userData.order) == 'object' && userData.order instanceof Array ? userData.order : [];
               // Create random id for check
-              var checkId = helpers.createRandomString(20);
+              var orderId = helpers.createRandomString(20);
 
-              // Create check object including userPhone
-              var checkObject = {
-                'id' : checkId,
-                'userPhone' : userPhone,
+              // Create check object including userEmail
+              var orderObject = {
+                'id' : orderId,
+                'userEmail' : userEmail,
                 'protocol' : protocol,
                 'url' : url,
                 'method' : method,
@@ -842,17 +840,17 @@ handlers._checks.post = function(data,callback){
               };
 
               // Save the object
-              _data.create('checks',checkId,checkObject,function(err){
+              _data.create('order',orderId,orderObject,function(err){
                 if(!err){
                   // Add check id to the user's object
-                  userData.checks = userChecks;
-                  userData.checks.push(checkId);
+                  userData.order = userOrder;
+                  userData.order.push(orderId);
 
                   // Save the new user data
-                  _data.update('users',userPhone,userData,function(err){
+                  _data.update('users',userEmail,userData,function(err){
                     if(!err){
                       // Return the data about the new check
-                      callback(200,checkObject);
+                      callback(200,orderObject);
                     } else {
                       callback(500,{'Error' : 'Could not update the user with the new check.'});
                     }
@@ -861,19 +859,10 @@ handlers._checks.post = function(data,callback){
                   callback(500,{'Error' : 'Could not create the new check'});
                 }
               });
-
-
-
-            } else {
-              callback(400,{'Error' : 'The user already has the maximum number of checks ('+config.maxChecks+').'})
-            }
-
-
           } else {
             callback(403);
           }
         });
-
 
       } else {
         callback(403);
@@ -887,17 +876,17 @@ handlers._checks.post = function(data,callback){
 // Checks - get
 // Required data: id
 // Optional data: none
-handlers._checks.get = function(data,callback){
+handlers._order.get = function(data,callback){
   // Check that id is valid
   var id = typeof(data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length == 20 ? data.queryStringObject.id.trim() : false;
   if(id){
     // Lookup the check
-    _data.read('checks',id,function(err,checkData){
+    _data.read('order',id,function(err,checkData){
       if(!err && checkData){
         // Get the token that sent the request
         var token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
         // Verify that the given token is valid and belongs to the user who created the check
-        handlers._tokens.verifyToken(token,checkData.userPhone,function(tokenIsValid){
+        handlers._tokens.verifyToken(token,checkData.userEmail,function(tokenIsValid){
           if(tokenIsValid){
             // Return check data
             callback(200,checkData);
@@ -917,7 +906,7 @@ handlers._checks.get = function(data,callback){
 // Checks - put
 // Required data: id
 // Optional data: protocol,url,method,successCodes,timeoutSeconds (one must be sent)
-handlers._checks.put = function(data,callback){
+handlers._order.put = function(data,callback){
   // Check for required field
   var id = typeof(data.payload.id) == 'string' && data.payload.id.trim().length == 20 ? data.payload.id.trim() : false;
 
@@ -933,12 +922,12 @@ handlers._checks.put = function(data,callback){
     // Error if nothing is sent to update
     if(protocol || url || method || successCodes || timeoutSeconds){
       // Lookup the check
-      _data.read('checks',id,function(err,checkData){
+      _data.read('order',id,function(err,checkData){
         if(!err && checkData){
           // Get the token that sent the request
           var token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
           // Verify that the given token is valid and belongs to the user who created the check
-          handlers._tokens.verifyToken(token,checkData.userPhone,function(tokenIsValid){
+          handlers._tokens.verifyToken(token,checkData.userEmail,function(tokenIsValid){
             if(tokenIsValid){
               // Update check data where necessary
               if(protocol){
@@ -958,7 +947,7 @@ handlers._checks.put = function(data,callback){
               }
 
               // Store the new updates
-              _data.update('checks',id,checkData,function(err){
+              _data.update('order',id,checkData,function(err){
                 if(!err){
                   callback(200);
                 } else {
@@ -985,34 +974,34 @@ handlers._checks.put = function(data,callback){
 // Checks - delete
 // Required data: id
 // Optional data: none
-handlers._checks.delete = function(data,callback){
+handlers._order.delete = function(data,callback){
   // Check that id is valid
   var id = typeof(data.queryStringObject.id) == 'string' && data.queryStringObject.id.trim().length == 20 ? data.queryStringObject.id.trim() : false;
   if(id){
     // Lookup the check
-    _data.read('checks',id,function(err,checkData){
+    _data.read('order',id,function(err,checkData){
       if(!err && checkData){
         // Get the token that sent the request
         var token = typeof(data.headers.token) == 'string' ? data.headers.token : false;
         // Verify that the given token is valid and belongs to the user who created the check
-        handlers._tokens.verifyToken(token,checkData.userPhone,function(tokenIsValid){
+        handlers._tokens.verifyToken(token,checkData.userEmail,function(tokenIsValid){
           if(tokenIsValid){
 
             // Delete the check data
-            _data.delete('checks',id,function(err){
+            _data.delete('order',id,function(err){
               if(!err){
-                // Lookup the user's object to get all their checks
-                _data.read('users',checkData.userPhone,function(err,userData){
+                // Lookup the user's object to get all their order
+                _data.read('users',checkData.userEmail,function(err,userData){
                   if(!err){
-                    var userChecks = typeof(userData.checks) == 'object' && userData.checks instanceof Array ? userData.checks : [];
+                    var userChecks = typeof(userData.order) == 'object' && userData.order instanceof Array ? userData.order : [];
 
-                    // Remove the deleted check from their list of checks
+                    // Remove the deleted check from their list of order
                     var checkPosition = userChecks.indexOf(id);
                     if(checkPosition > -1){
                       userChecks.splice(checkPosition,1);
                       // Re-save the user's data
-                      userData.checks = userChecks;
-                      _data.update('users',checkData.userPhone,userData,function(err){
+                      userData.order = userChecks;
+                      _data.update('users',checkData.userEmail,userData,function(err){
                         if(!err){
                           callback(200);
                         } else {
@@ -1023,7 +1012,7 @@ handlers._checks.delete = function(data,callback){
                       callback(500,{"Error" : "Could not find the check on the user's object, so could not remove it."});
                     }
                   } else {
-                    callback(500,{"Error" : "Could not find the user who created the check, so could not remove the check from the list of checks on their user object."});
+                    callback(500,{"Error" : "Could not find the user who created the check, so could not remove the check from the list of order on their user object."});
                   }
                 });
               } else {
--- ex4/lib/helpers.js
+++ ex4/lib/helpers.js
@@ -75,64 +75,6 @@ helpers.datastore = function(data, callback){
   }
 };
 
-/*
-helpers.sendTwilioSms = function(phone,msg,callback){
-  // Validate parameters
-  phone = typeof(phone) == 'string' && phone.trim().length == 10 ? phone.trim() : false;
-  msg = typeof(msg) == 'string' && msg.trim().length > 0 && msg.trim().length <= 1600 ? msg.trim() : false;
-  if(phone && msg){
-
-    // Configure the request payload
-    var payload = {
-      'From' : config.twilio.fromPhone,
-      'To' : '+1'+phone,
-      'Body' : msg
-    };
-    var stringPayload = querystring.stringify(payload);
-
-
-    // Configure the request details
-    var requestDetails = {
-      'protocol' : 'https:',
-      'hostname' : 'api.twilio.com',
-      'method' : 'POST',
-      'path' : '/2010-04-01/Accounts/'+config.twilio.accountSid+'/Messages.json',
-      'auth' : config.twilio.accountSid+':'+config.twilio.authToken,
-      'headers' : {
-        'Content-Type' : 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(stringPayload)
-      }
-    };
-
-    // Instantiate the request object
-    var req = https.request(requestDetails,function(res){
-        // Grab the status of the sent request
-        var status =  res.statusCode;
-        // Callback successfully if the request went through
-        if(status == 200 || status == 201){
-          callback(false);
-        } else {
-          callback('Status code returned was '+status);
-        }
-    });
-
-    // Bind to the error event so it doesn't get thrown
-    req.on('error',function(e){
-      callback(e);
-    });
-
-    // Add the payload
-    req.write(stringPayload);
-
-    // End the request
-    req.end();
-
-  } else {
-    callback('Given parameters were missing or invalid');
-  }
-};
-*/
-
 // Get the string content of a template, and use provided data for string interpolation
 helpers.getTemplate = function(templateName,data,callback){
   templateName = typeof(templateName) == 'string' && templateName.length > 0 ? templateName : false;
