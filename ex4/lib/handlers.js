6f41bb222b61643d996a8bf38a5db40c117106a8
Richard L. Jackson <denezt@yahoo.com>
2021-01-15 16:41:59 +0100

    Updating code - 1610725319

--- ex4/lib/handlers.js
+++ ex4/lib/handlers.js
@@ -1058,7 +1058,7 @@ handlers._cart.post = function(data,callback){
           if(!err && userData){
             var userOrder = typeof(userData.order) == 'object' && userData.order instanceof Array ? userData.order : [];
             for (var i = 0; i < userOrder.length; i++) {
-              console.log(userOrder);
+              console.log(userOrder[i]);
             }
 
           } else {
