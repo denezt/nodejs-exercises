aaabd3ed7fc0d860eec58b8c249f3df06d3664d4
Richard L. Jackson <denezt@yahoo.com>
2021-01-17 14:05:53 +0100

    Updating code - 1610888753

--- ex4/lib/handlers.js
+++ ex4/lib/handlers.js
@@ -292,6 +292,36 @@ handlers.orderEdit = function(data,callback){
   }
 };
 
+// Edit a Check
+handlers.externalApiSettings = function(data,callback){
+  // Reject any request that isn't a GET
+  if(data.method == 'get'){
+    // Prepare data for interpolation
+    var templateData = {
+      'head.title' : 'External API Settings',
+      'body.class' : 'externalApiSettings'
+    };
+    // Read in a template as a string
+    helpers.getTemplate('externalApiSettings',templateData,function(err,str){
+      if(!err && str){
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str,templateData,function(err,str){
+          if(!err && str){
+            // Return that page as HTML
+            callback(200,str,'html');
+          } else {
+            callback(500,undefined,'html');
+          }
+        });
+      } else {
+        callback(500,undefined,'html');
+      }
+    });
+  } else {
+    callback(405,undefined,'html');
+  }
+};
+
 // Favicon
 handlers.favicon = function(data,callback){
   // Reject any request that isn't a GET
