418d0ce4f219005faf20e45dfe2e675915b6cd1e
Richard L. Jackson <denezt@yahoo.com>
2021-01-25 21:28:19 +0100

    Updating code - 1611606499

--- ex4/public/app.js
+++ ex4/public/app.js
@@ -475,7 +475,7 @@ app.loadOrderConfirmPage = function(){
           var totalCost = 0.0;
 
           for (var i = 0; i < menuNameArray.length; i++) {
-            if (typeof(menuNameArray[i]) != 'undefined'){
+            if (typeof(responsePayload.menuItems[menuNameArray[i]]) != 'undefined'){
               if (responsePayload.menuItems[menuNameArray[i]]){
                 console.log('Customer bought: ' + menuItemArray.items[i].name + ' =>' + menuNameArray[i]);
                 menuItems += '[+] ' + menuItemArray.items[i].name + '\n';
