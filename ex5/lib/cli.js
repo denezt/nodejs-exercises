da2de06a082ce10d1bdb6940d4dd781177df3be7
URUS CG <uruscg@gmail.com>
2021-01-30 07:38:30 +0000

    updating cli.js with new functionality

--- ex5/lib/cli.js
+++ ex5/lib/cli.js
@@ -1,21 +1,122 @@
 /*
-* CLI Related Tasks
-*/
+ * CLI-related tasks
+ *
+ */
 
 // Dependencies
-const readline = require('readline');
-const util = require('util');
-const debug = util.debuglog('cli');
-const events = require('events');
+var readline = require('readline');
+var util = require('util');
+var debug = util.debuglog('cli');
+var events = require('events');
 class _events extends events{};
-e = new _events();
+var e = new _events();
 
+// Instantiate the cli module object
 var cli = {};
 
-cli.init = function(){
+// Input handlers
+e.on('help',function(str){
+  cli.responders.help();
+});
 
+e.on('exit',function(str){
+  cli.responders.exit();
+});
+
+e.on('menu items',function(str){
+  cli.responders.menu();
+});
+
+e.on('view menu items',function(str){
+  cli.responders.menu();
+});
+
+// Responders object
+cli.responders = {};
+
+// Help
+cli.responders.help = function(){
+  console.log("You asked for help");
+};
+
+// Current Menu items
+cli.responders.menu = function(){
+  console.log("You asked for menu items");
+};
+
+// Exit
+cli.responders.exit = function(){
+  process.exit(0);
 };
 
 
+
+// Input processor
+cli.processInput = function(str){
+  str = typeof(str) == 'string' && str.trim().length > 0 ? str.trim() : false;
+  // Only process the input if the user actually wrote something, otherwise ignore it
+  if(str){
+    // Codify the unique strings that identify the different unique questions allowed be the asked
+    var uniqueInputs = [
+      'help',
+      'exit',
+      'menu items',
+      'view menu items'
+    ];
+
+    // Go through the possible inputs, emit event when a match is found
+    var matchFound = false;
+    var counter = 0;
+    uniqueInputs.some(function(input){
+      if(str.toLowerCase().indexOf(input) > -1){
+        matchFound = true;
+        // Emit event matching the unique input, and include the full string given
+        e.emit(input,str);
+        return true;
+      }
+    });
+
+    // If no match is found, tell the user to try again
+    if(!matchFound){
+      console.log("Sorry, try again");
+    }
+
+  }
+};
+
+// Init script
+cli.init = function(){
+
+  // Send to console, in dark blue
+  console.log('\x1b[34m%s\x1b[0m','The CLI is running');
+
+  // Start the interface
+  var _interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: ''
+  });
+
+  // Create an initial prompt
+  _interface.prompt();
+
+  // Handle each line of input separately
+  _interface.on('line', function(str){
+
+    // Send to the input processor
+    cli.processInput(str);
+
+    // Re-initialize the prompt afterwards
+    _interface.prompt();
+  });
+
+  // If the user stops the CLI, kill the associated process
+  _interface.on('close', function(){
+    process.exit(0);
+  });
+
+};
+
+// Export the module
 module.exports = cli;
 
