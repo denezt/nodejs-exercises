2d1ebf483e929f482b7f9d33e29f5ea32db2226b
Richard L. Jackson <denezt@yahoo.com>
2021-01-31 21:30:35 +0100

    Updating code - 1612125034

--- ex5/lib/handlers.js
+++ ex5/lib/handlers.js
@@ -1304,7 +1304,7 @@ handlers._pay.post = function(data, callback){
                   var id = 1;
                   for (var variable in itemsArray) {
                     if (variable == true){
-                      console.log('\033[34m' + itemsObj.items[id] + '\033[0m');
+                      console.log('\033[34m' + itemsObj.items[id].name + '\033[0m');
                     }
                     id++;
                   }
