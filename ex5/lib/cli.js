96c1904ccb12dbbc34e638231b269b65b4c00b24
Richard L. Jackson <denezt@yahoo.com>
2021-01-31 15:33:34 +0100

    Updating code - 1612103614

--- ex5/lib/cli.js
+++ ex5/lib/cli.js
@@ -94,10 +94,10 @@ cli.responders.signups = function(){
   fs.readFile('../.data/records/users_list.json', 'utf8', function(err,data){
     if(!err && data){
       var parsedData = helpers.parseJsonToObject(data);
-      console.log(data);
       return parsedData;
     }
   });
+  console.log('All Signups');
 };
 
 cli.responders.orders = function(){
