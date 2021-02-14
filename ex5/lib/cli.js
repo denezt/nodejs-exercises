112b51cdf423f52c93e6daf8181d545de9b00b77
Richard L. Jackson <denezt@yahoo.com>
2021-02-14 19:01:05 +0100

    Updating source code

--- ex5/lib/cli.js
+++ ex5/lib/cli.js
@@ -172,7 +172,7 @@ cli.responders.order = function(str){
       if (!err){
         for (var i = 0; i < menuItem.length; i++) {
           if (menuItem[i]){
-            console.log('Order Item:\t' + menuitem[i]);
+            console.log('Order Item:\t' + menuitem[i + 1]);
           }
         }
       }
