b98da5b00085d9de923a82d459dd81b01e9a1060
Richard L. Jackson <denezt@yahoo.com>
2021-02-14 12:27:17 +0100

    Updating source code

--- ex5/lib/cli.js
+++ ex5/lib/cli.js
@@ -122,7 +122,7 @@ cli.responders.user = function(str){
   var arr = typeof(str) == 'string' ? str.split('--') : false;
   var userInfo = typeof(arr[1]) == 'string' && arr[1].trim().length > 0  ? arr[1].trim() : false;
   if (userInfo){
-    var userEmail = userInfo.split(' ')[1].replace('@','_').replace('.com','');
+    var userEmail = userInfo.split(' ')[1].replace('@','_').replace('.','_');
     console.log("showing user: ",userEmail);
     _data.read('users',userEmail,function(err,userData){
       if (err){
