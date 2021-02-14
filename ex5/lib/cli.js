c7337732f2ab5328ac98e9d5a8fb311ede78ad8a
Richard L. Jackson <denezt@yahoo.com>
2021-02-14 12:38:04 +0100

    Updating source code

--- ex5/lib/cli.js
+++ ex5/lib/cli.js
@@ -122,19 +122,15 @@ cli.responders.user = function(str){
   var arr = typeof(str) == 'string' ? str.split('--') : false;
   var userInfo = typeof(arr[1]) == 'string' && arr[1].trim().length > 0  ? arr[1].trim() : false;
   if (userInfo){
-    var emailArg = (userInfo.split(' ')[0] == '--email') ? true : false;
-    if (emailArg){
-      var userEmail = userInfo.split(' ')[1].replace('@','_').replace('.','_');
-      console.log("showing user: ",userEmail);
-      _data.read('users',userEmail,function(err,userData){
-        if (!err){
-          delete userData.hashedPassword;
-          console.log(userData);
-        }
-      });
-    } else {
-      console.log('Missing or invalid Parameter was entered');
-    }
+    console.log(userInfo.split(' ')[0]);
+    var userEmail = userInfo.split(' ')[1].replace('@','_').replace('.','_');
+    console.log("showing user: ",userEmail);
+    _data.read('users',userEmail,function(err,userData){
+      if (!err){
+        delete userData.hashedPassword;
+        console.log(userData);
+      }
+    });
   }
 };
 
