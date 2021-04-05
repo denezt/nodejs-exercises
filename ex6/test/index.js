a0964355f8862630875d9145f7d6f95e522529a7
Richard L. Jackson <denezt@yahoo.com>
2021-04-05 16:07:24 +0200

    Update source code - 1617631644

--- ex6/test/index.js
+++ ex6/test/index.js
@@ -4,5 +4,9 @@
 
 // Application logic for the test runner
 var _test = require('./runner');
+// var os = require('os');
+
+// var networkInterfaces = os.networkInterfaces();
+// console.log(networkInterfaces);
 
 module.exports = _test;
